6b0b7d76bdfeaac61829fcfafc4416819697d47a
BimxyzDev <bimaputra436123@gmail.com>
2026-01-17 13:28:06 +0700

    Update via Panel Manager

--- api/panel.js
+++ api/panel.js
@@ -1,5 +1,5 @@
-export const PANEL_URL = "https://pterodactyl.priv.bimxyz.my.id";
-export const API_KEY = "ptla_jC3KkZ12El8SlarR4qJnm8ERZNjgiQU6iwuaO8LoEnj";
+export const PANEL_URL = "https://pterodactyl.gw.bimxyz.my.id";
+export const API_KEY = "ptla_zupsdHGzCgDial4iBRdLTgzrDbsz78iYWh1b2kAIBAC";
 export const NODE_ID = "1";
 export const NEST_ID = "5";
 export const EGG_ID = "15";
